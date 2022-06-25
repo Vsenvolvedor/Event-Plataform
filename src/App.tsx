440dc7f2b4c8203bbb3b7ceac611acda165530aa
Vitor <catchtherainbow33@gmail.com>
2022-06-24 21:33:16 -0300

    Pré-criando alguns componentes da interface do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
-
 function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY);
-
-  console.log(data)
 
   return (
-    <h1>
-      Hello world
-    </h1>
+    <ul>
+    
+    </ul>
   )
 }
 

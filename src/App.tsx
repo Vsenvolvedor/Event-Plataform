b347519edbb5e0d24169e58c3605a9101b2b47b4
Vitor <catchtherainbow33@gmail.com>
2022-06-24 21:07:54 -0300

    Primeiros testes com Apollo, requisitando dados da API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+
+      teacher {
+        name
+      }
+    }
+  }
+`
 
 function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+
+  console.log(data)
 
   return (
     <h1>

fd4914434418b968635a750011d8b23617d1868f
Vitor <catchtherainbow33@gmail.com>
2022-06-24 22:26:51 -0300

    Recebendo propriedades no componente Lesson e criando uma interface com o typescript

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,13 @@
-export function Lesson() {
+import { CheckCircle } from 'phosphor-react';
+
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: 'live' | 'class';
+}
+
+export function Lesson(prosp:LessonProps) {
   return (
     <a href="#">
       <span className="text-gray-300">
@@ -6,7 +15,8 @@ export function Lesson() {
       </span>
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium">
+          <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+            <CheckCircle size={20} />
             Conteúdo liberado
           </span>
           <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">

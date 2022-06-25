522a803d8ca474abcfa910395ebaef361eaebc5f
Vitor <catchtherainbow33@gmail.com>
2022-06-25 17:22:23 -0300

    Requisitando os dados do CMS para uso no componente video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,47 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 import '@vime/core/themes/default.css';
 
-export function Video() {
+const GET_LESSON_BY_SLUG  = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title 
+      videoId
+      description
+      teacher {
+        bio 
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props:VideoProps) {
+  const { data } = useQuery(GET_LESSON_BY_SLUG, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
